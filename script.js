6ee9aa9b0a93ab82eb6f5c48ede91098b84332b1
HoangNguyen <hoangnh40@fpt.com.vn>
2022-05-19 10:54:50 +0700

    clear code

--- script.js
+++ script.js
@@ -2,14 +2,3 @@ import { attach } from "./store.js";
 import App from './component/App.js'
 
 attach(App, document.getElementById('root'));
-
-// import html from './core.js';
-
-// const cars = ['BMW', 'Porsche', 'Mazda'];
-
-// const output = html`
-//   <h3>TEST ${false}</h3>
-//   <ul>
-//     ${cars.map(car => `<li>${car}</li>`).join('')}
-//   </ul>
-// `
